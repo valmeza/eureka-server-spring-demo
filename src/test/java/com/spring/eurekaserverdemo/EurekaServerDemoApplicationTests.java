54849c6373474798184ca6046932fb5d6d71c6bb
Valeria Meza <valeriameza173@gmail.com>
2021-03-17 20:04:01 -0500

    add skeleton project.

---
+++ src/test/java/com/spring/eurekaserverdemo/EurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.eurekaserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
